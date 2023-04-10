30f08c92091797028b8feb461b26cb1d5a7a8579
scott1991 <cakepbm@gmail.com>
2023-04-10 15:02:24 +0800

    增加阿夸,久遠

---
+++ commands/streamers/yt/tama.js
@@ -0,0 +1,37 @@
+const { TwitchChatCommand } = require('twitch-commando');
+const util = require('util');
+const moment = require('moment');
+const ytSvc = require('../../../service/ytSvc');
+const momentDurationFormatSetup = require("moment-duration-format");
+momentDurationFormatSetup(moment);
+moment.locale('zh-tw');
+
+const {RateLimitedTwitchChatCommand} = require('../../../service/rateLimited');
+
+class ViwersCommand extends RateLimitedTwitchChatCommand {
+  constructor(client) {
+    super(client, {
+      name: '!久遠人數',
+      aliases: [ '!Tama人數', '!QuonTama人數' ],
+      group: 'streamers',
+      description: ''
+    });
+  }
+
+  async delayRun(msg) {
+    try {
+      let info = await ytSvc.getLiveInfoByChannelId('UCBC7vYFNQoGPupe5NxPG4Bw');
+      if ( info.actualEndTime ){
+        msg.reply('久遠たま現在沒開台，上次關台是'+ moment(info.actualEndTime).format("yyyy-MM-DD HH:mm") );   
+      }else{
+        msg.reply(`久遠たま台現在有:${info.currentViewers}人`);
+      }
+       
+    } catch (e) {
+      msg.reply('我找不到欸');
+      console.log(util.inspect(e, { depth: 3 }))
+    }
+  }
+}
+
+module.exports = ViwersCommand;

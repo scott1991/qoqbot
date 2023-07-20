d4ffbe3d314c99f03ec328ff1eeb035368ab69dc
scott1991 <cakepbm@gmail.com>
2023-07-21 00:43:22 +0800

    Change time command to use call bot add followTime remove bad words in newTurtle

--- commands/querys/followTime.js
+++ commands/querys/followTime.js
@@ -11,9 +11,9 @@ const {RateLimitedTwitchChatCommand} = require('../../service/rateLimited');
 class TwitchAccountRegistrationTime extends RateLimitedTwitchChatCommand {
   constructor(client) {
     super(client, {
-      name: '!註冊時間',
+      name: '!追隨時間',
       group: 'querys',
-      description: 'querys account registration time',
+      description: '',
       args: [
         {
           name: 'arg1'
@@ -24,20 +24,9 @@ class TwitchAccountRegistrationTime extends RateLimitedTwitchChatCommand {
 
   async delayRun(msg) {
     let arg1 = '' ;
-    let regdt;
     try {
       let name = arg1 ? arg1 : msg.author.username;
-      this.client.say(msg.channel.name, '!accountage ' + name); // 在聊天室中發送訊息
-      /*
-      let userData = await twitchSvc.getUserByName(name);
-      if (userData.body.createdAt) {
-        regdt = moment(userData.body.createdAt);
-        let duration = moment.duration(moment().diff(regdt)).format("y [年] M[月] d[天]");
-        msg.reply(arg1 + '是 ' + regdt.format("yyyy-MM-DD") + ' 也就是 ' + duration + '前註冊的');
-      } else {
-        msg.reply('我找不到欸');
-      }
-      */
+      this.client.say(msg.channel.name, '!followage ' + name); 
     } catch (e) {
       msg.reply('我找不到欸');
       console.log(util.inspect(e, { depth: 3 }))

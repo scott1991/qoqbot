d71947f3e05509eaf11c1c68733115dd046a200a
scott1991 <cakepbm@gmail.com>
2024-05-08 01:51:51 +0800

    Change godtone to twitch

--- commands/streamers/twitch/godtone.js
+++ commands/streamers/twitch/godtone.js
@@ -1,7 +1,7 @@
 const { TwitchChatCommand } = require('twitch-commando');
 const util = require('util');
 const moment = require('moment');
-const ytSvc = require('../../../service/ytSvc');
+const twitchSvc = require('../../../service/twitchSvc');
 const momentDurationFormatSetup = require("moment-duration-format");
 momentDurationFormatSetup(moment);
 moment.locale('zh-tw');
@@ -11,7 +11,7 @@ const {RateLimitedTwitchChatCommand} = require('../../../service/rateLimited');
 class ViwersCommand extends RateLimitedTwitchChatCommand {
   constructor(client) {
     super(client, {
-      name: '!0董神人數',
+      name: '!董神人數',
       aliases: [ '!統神人數', '!阿舟人數', '!舟董人數', '!阿航人數', '!阿夯人數' ],
       group: 'streamers',
       description: ''
@@ -20,11 +20,15 @@ class ViwersCommand extends RateLimitedTwitchChatCommand {
 
   async delayRun(msg) {
     try {
-      let info = await ytSvc.getLiveInfoByChannelId('UCmJwfvC-1omeZ_31Cb6vawA');
-      if ( info.actualEndTime ){
-        msg.reply('董神現在沒開台，上次關台是'+ moment(info.actualEndTime).format("yyyy-MM-DD HH:mm"));   
+      let info = await twitchSvc.getLiveViewersCountByName('asiagodtonegg3be0');
+      if ( info.viewers ){
+        msg.reply(`董神台現在有:${info.viewers}人`);
       }else{
-        msg.reply(`董神台現在有:${info.currentViewers}人`);
+        if ( info.time ){
+          msg.reply('董神現在沒開台，上次關台是'+ moment( info.time).format("yyyy-MM-DD HH:mm"));  
+        }else{
+          msg.reply('董神現在沒開台');
+        } 
       }
        
     } catch (e) {

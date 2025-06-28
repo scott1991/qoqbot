39110e3ccaf0b415d24cfae1b3b1092cf7c68864
scott1991 <cakepbm@gmail.com>
2025-06-28 16:22:51 +0800

    @Add YtViewerCommand and configuration for viewer count commands

---
+++ commands/test/config.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    displayName: '百鬼あやめ',
+    command: '!百鬼人數a',
+    aliases: ['!Ayame人數', '!ayame人數'],
+    channelId: 'UC7fk0CB07ly8oSl0aqKkqFg'
+  },
+  {
+    displayName: '湊あくあ',
+    command: '!Aqua人數',
+    aliases: ['!あくあ人數', '!阿垮人數', '!阿夸人數', '!aqua人數'],
+    channelId: 'UC1opHUrw8rvnsadT-iGp7Cg'
+  }
+];

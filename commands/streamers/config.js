a127d468dd4b3c715f8f8448bf22afa202b2b07e
scott1991 <cakepbm@gmail.com>
2025-11-03 01:34:22 +0800

    @remove viewer count commands for various streamers on YouTube and Twitch use config.js and ViewerCommand.js instead

---
+++ commands/streamers/config.js
@@ -0,0 +1,331 @@
+module.exports = [
+  {
+    displayName: '神之愛',
+    command: '!神之愛人數',
+    aliases: ['!eddy人數', '!Eddy人數', '!EddyTommy人數', '!愛哥人數', '!愛醬人數', 'edytommy人數'],
+    youtubeChannelId: 'UCiJ0I4szok8aMxsWU8iTF_Q',
+    twitchUsername: 'eddytommy30'
+  },
+  {
+    displayName: '9QOQ',
+    command: '!9QOQ人數',
+    aliases: ['!9Q人數', '!睪慶人數', '!陳澤慶人數'],
+    youtubeChannelId: null,
+    twitchUsername: '9qoq'
+  },
+  {
+    displayName: '艾里',
+    command: '!ailez人數',
+    aliases: ['!Ailez人數', '!艾里人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'ailez'
+  },
+  {
+    displayName: '赤鬼伯伯',
+    command: '!赤鬼人數',
+    aliases: ['!赤鬼伯伯人數', '!老拍拍人數', '!老拍人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'akaonikou1207'
+  },
+  {
+    displayName: 'anny',
+    command: '!anny人數',
+    aliases: ['!Anny人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'anny'
+  },
+  {
+    displayName: '玩玩吉',
+    command: '!玩玩吉人數',
+    aliases: ['!arthur8612人數', '!玩神人數', '!wawag人數', '!wawa g人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'arthur8612'
+  },
+  {
+    displayName: '百鬼あやめ',
+    command: '!百鬼人數',
+    aliases: ['!Ayame人數', '!ayame人數'],
+    youtubeChannelId: 'UC7fk0CB07ly8oSl0aqKkqFg',
+    twitchUsername: 'nakiriayame_hololive'
+  },
+  {
+    displayName: '餅乾',
+    command: '!餅乾好吃人數',
+    aliases: ['!餅乾人數', '!cookie人數', '!Cookie人數', '!cookiegoodgood人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'cookiegoodgood'
+  },
+  {
+    displayName: '朵莉',
+    command: '!朵莉人數',
+    aliases: ['!dorirodo人數', '!Dorirodo人數', '!Dori人數', '!DORI人數', '!都都人數', '!都z人數', '!都Z人數', '!朵z人數', '!朵Z人數'],
+    youtubeChannelId: 'UCMf_bqEhD0lLaLUkb2Uu8PA',
+    twitchUsername: 'dorirodo'
+  },
+  {
+    displayName: 'ET',
+    command: '!依渟人數',
+    aliases: ['!ET人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'et_1231'
+  },
+  {
+    displayName: '董神',
+    command: '!董神人數',
+    aliases: ['!統神人數', '!阿舟人數', '!舟董人數', '!阿航人數', '!阿夯人數'],
+    youtubeChannelId: 'UCmJwfvC-1omeZ_31Cb6vawA',
+    twitchUsername: 'asiagodtonegg3be0'
+  },
+  {
+    displayName: '冠賢',
+    command: '!冠賢人數',
+    aliases: ['!阿賢人數', '!吳冠賢人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'gunguno'
+  },
+  {
+    displayName: '盜ET',
+    command: '!盜ET人數',
+    aliases: ['!稻乙緹人數', '!iiti人數', '!Iiti人數', '!稻ET人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'iitifox'
+  },
+  {
+    displayName: '赤見かるび',
+    command: '!赤見人數',
+    aliases: ['!akamikarubi人數', '!赤見かるび人數', '!かるび人數', '!akami人數', '!karubi人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'akamikarubi'
+  },
+  {
+    displayName: '蘆葦',
+    command: '!蘆葦人數',
+    aliases: ['!luweibaby人數', '!蘆筍人數', '!弓神人數', '!詐欺師人數', '!蘆葦伯人數', '!滷肉飯人數', '!滷雞腿人數', '!蘆筍炒肉絲人數', '!滷肉便當人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'luweibaby'
+  },
+  {
+    displayName: '奶茶蜜',
+    command: '!奶茶蜜人數',
+    aliases: ['!奶茶密人數', '!ねね人數', '!羅甄甄人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'milkteadotcom'
+  },
+  {
+    displayName: '湊あくあ',
+    command: '!阿夸圖奇人數',
+    aliases: ['!あくあ圖奇人數', '!阿垮圖奇人數', '!阿夸twitch人數', '!aqua圖奇人數', '!Aqua圖奇人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'minatoaquach_hololive'
+  },
+  {
+    displayName: '小雨',
+    command: '!小雨人數',
+    aliases: ['!mitsukirain人數', '!雨涵人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'mitsukirain'
+  },
+  {
+    displayName: '米豬捏',
+    command: '!米豬捏人數',
+    aliases: ['!mizune__人數', '!mizune人數', '!Mizune人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'mizune__'
+  },
+  {
+    displayName: '伊織もえ',
+    command: '!moe人數',
+    aliases: ['!一枝萌人數', '!moe_iori人數', '一直萌人數', '依織萌人數', '伊織もえ人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'moe_iori'
+  },
+  {
+    displayName: 'モ二力',
+    command: '!monika927人數',
+    aliases: ['!monika人數', '!莫妮卡人數', '!モ二力人數', '!毛二力人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'monika927'
+  },
+  {
+    displayName: '羅傑',
+    command: '!羅傑人數',
+    aliases: ['!roger人數', '!羅總人數', '!喇傑人數', '!Roger人數', '!北投超威人數'],
+    youtubeChannelId: 'UCiEm9noegBIb-AzjqpxKffA',
+    twitchUsername: 'roger9527'
+  },
+  {
+    displayName: '龜狗',
+    command: '!龜狗人數',
+    aliases: ['!小龜人數', '!龜哥人數', '!想龜人數', '!桂格人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'sweetcampercs'
+  },
+  {
+    displayName: 'Taco',
+    command: '!Taco人數',
+    aliases: ['!taco人數', '!tacoo2023人數', '!他口人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'tacoo2023'
+  },
+  {
+    displayName: '泰勒',
+    command: '!泰勒人數',
+    aliases: ['!taylor人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'taylorjen'
+  },
+  {
+    displayName: 'Toki',
+    command: '!toki人數',
+    aliases: ['!Toki人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'toki_1717'
+  },
+  {
+    displayName: 'neuro',
+    command: '!vedal987人數',
+    aliases: ['!neuro人數', '!牛肉人數', '!444人數'],
+    youtubeChannelId: null,
+    twitchUsername: 'vedal987'
+  },
+  {
+    displayName: '湊あくあ',
+    command: '!Aqua人數',
+    aliases: ['!あくあ人數', '!阿垮人數', '!阿夸人數', '!aqua人數'],
+    youtubeChannelId: 'UC1opHUrw8rvnsadT-iGp7Cg',
+    twitchUsername: null
+  },
+  {
+    displayName: '獅白ぼたん',
+    command: '!Botan人數',
+    aliases: ['!botan人數', '!獅白人數', '!獅白ぼたん人數'],
+    youtubeChannelId: 'UCUKD-uaobj9jiqB-VXt71mA',
+    twitchUsername: null
+  },
+  {
+    displayName: '日南',
+    command: '!日南人數',
+    aliases: ['!canan人數'],
+    youtubeChannelId: 'UCX8fFzD1Kdi8uCSmbOce08w',
+    twitchUsername: null
+  },
+  {
+    displayName: '沙花叉クロヱ',
+    command: '!虎鯨人數',
+    aliases: ['!沙花叉クロヱ人數', '!沙花叉人數', '!クロヱ人數', '!クロたん人數', '!Chloe人數', '!chloe人數'],
+    youtubeChannelId: 'UCIBY1ollUsauvVi4hW4cumw',
+    twitchUsername: null
+  },
+  {
+    displayName: 'FUWAMOCO',
+    command: '!fuwamoco人數',
+    aliases: ['!Fuwamoco人數', '!FUWAMOCO人數', '!baubau人數', '!BAUBAU人數'],
+    youtubeChannelId: 'UCt9H_RpQzhxzlyBxFqrdHqA',
+    twitchUsername: null
+  },
+  {
+    displayName: 'Gawr Gura',
+    command: '!gura人數',
+    aliases: ['!Gura人數', '!古拉人數', '!鯊魚人數', '!鯊鯊人數'],
+    youtubeChannelId: 'UCoSrY_IQQVpmIRZ9Xf-y93g',
+    twitchUsername: null
+  },
+  {
+    displayName: '戌神ころね',
+    command: '!Korone人數',
+    aliases: ['!korone人數', '!麵包狗人數', '!熱狗人數', '!小狗人數', '!狗狗人數', '!戌神人數', '!沁音人數', '!戌神沁音人數', '!戌神ころね人數'],
+    youtubeChannelId: 'UChAnqc_AY5_I3Px5dig3X1Q',
+    twitchUsername: null
+  },
+  {
+    displayName: '姫森ルーナ',
+    command: '!luna人數',
+    aliases: ['!Luna人數', '!LUNA人數', '!姫森人數', '!ルーナ人數', '!姫森ルーナ人數'],
+    youtubeChannelId: 'UCa9Y57gfeY0Zro_noHRVrnw',
+    twitchUsername: null
+  },
+  {
+    displayName: '宝鐘マリン',
+    command: '!船長人數',
+    aliases: ['!馬鈴薯人數', '!瑪琳人數', '!寶鐘瑪琳人數'],
+    youtubeChannelId: 'UCCzUftO8KOVkV4wQG1vkUvg',
+    twitchUsername: null
+  },
+  {
+    displayName: '白銀ノエル',
+    command: '!Noel人數',
+    aliases: ['!白銀人數', '!白銀ノエル數', '!ノエル數', '!noel人數', '團長人數'],
+    youtubeChannelId: 'UCdyqAaZDKHXg4Ahi7VENThQ',
+    twitchUsername: null
+  },
+  {
+    displayName: '超負荷',
+    command: '!超負荷人數',
+    aliases: ['!負荷人數', '!張家盛人數', '!陣頭文化研究所所長人數', '!NL老公人數'],
+    youtubeChannelId: 'UCGXr9L5Kg77hLMZN4r8X8mw',
+    twitchUsername: 'sam1268'
+  },
+  {
+    displayName: '兎田ぺこら',
+    command: '!Pekora人數',
+    aliases: ['!pekora人數', '!兔田人數', '!配可拉人數', '!佩克拉人數'],
+    youtubeChannelId: 'UC1DCedRgGHBdm81E1llLhOQ',
+    twitchUsername: null
+  },
+  {
+    displayName: '結城さくな',
+    command: '!sakuna人數',
+    aliases: ['!Sakuna人數', '!結城人數', '!櫻奈人數', '!結城櫻奈人數'],
+    youtubeChannelId: 'UC1DCedRgGHBdm81E1llLhOQ',
+    twitchUsername: null
+  },
+  {
+    displayName: '紫咲シオン',
+    command: '!Shion人數',
+    aliases: ['!詩音人數', '!紫咲人數', '!紫咲シオン人數', '!紫咲詩音人數', '!小學生人數', '!蒜頭人數'],
+    youtubeChannelId: 'UCXTpFs_3PqI41qX2d9tL2Rw',
+    twitchUsername: null
+  },
+  {
+    displayName: '大空スバル',
+    command: '!Subaru人數',
+    aliases: ['!subaru人數', '!486人數', '!古神人數', '!鴨子人數', '!大空昴人數', '!大空人數', '!大空スバル人數'],
+    youtubeChannelId: 'UCvzGlP9oQwU--Y0r9id_jnA',
+    twitchUsername: null
+  },
+  {
+    displayName: '星街すいせい',
+    command: '!Suisei人數',
+    aliases: ['!星姐人數', '!阿星人數', '!彗彗人數', '!星街すいせい人數', '!彗星人數', '!星街彗星人數', '!星街人數'],
+    youtubeChannelId: 'UC5CwaMl1eIgY8h02uZw7u8A',
+    twitchUsername: null
+  },
+  {
+    displayName: '久遠たま',
+    command: '!久遠人數',
+    aliases: ['!Tama人數', '!QuonTama人數', '!久遠たま人數', '!久遠tama人數', '!tama人數'],
+    youtubeChannelId: 'UCBC7vYFNQoGPupe5NxPG4Bw',
+    twitchUsername: null
+  },
+  {
+    displayName: '常闇トワ',
+    command: '!Towa人數',
+    aliases: ['!常闇人數', '!常闇トワ人數', '!トワ人數', '!towa人數'],
+    youtubeChannelId: 'UC1uv2Oq6kNxgATlCiez59hw',
+    twitchUsername: null
+  },
+  {
+    displayName: 'Watson Amelia',
+    command: '!Watson人數',
+    aliases: ['!華生人數', '!Amelia人數', '!阿梅人數', '!花生人數', '!🥜人數', '!watson人數', '!amelia人數'],
+    youtubeChannelId: 'UCyl1z3jo3XHR1riLFKG5UAg',
+    twitchUsername: null
+  },
+  {
+    displayName: '團勳',
+    command: '!國動人數',
+    aliases: ['!陌生人人數', '!團勳人數'],
+    youtubeChannelId: 'UCw-N9mI3Om-AN2xgkEoSDsg',
+    twitchUsername: null
+  }
+];

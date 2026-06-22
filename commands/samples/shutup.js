330d78e046deffba8b6023a7356caf003da24f4c
Scott <cakepbm@gmail.com>
2026-06-23 01:43:03 +0800

    @Refactor chat command and update AI chat prompt for clarity and consistency

--- commands/samples/shutup.js
+++ commands/samples/shutup.js
@@ -12,7 +12,7 @@ class SampleCommand extends RateLimitedTwitchChatCommand {
   }
 
   async delayRun(msg) {
-    this.client.say(msg.channel.name, "https://i.imgur.com/hDU35z8.png"); // 在聊天室中發送訊息
+    // this.client.say(msg.channel.name, "https://i.imgur.com/hDU35z8.png"); // 在聊天室中發送訊息
   }
 }
 module.exports = SampleCommand;

5bfb5804fbc71769e935c34797e537c2e1cff235
Scott <cakepbm@gmail.com>
2026-06-30 22:46:10 +0800

    @Add FIFA query commands for after tomorrow and update rate limiting method

--- commands/querys/fifaToday.js
+++ commands/querys/fifaToday.js
@@ -2,19 +2,41 @@ const util = require('util');
 const fifaSvc = require('../../service/fifaSvc');
 const {RateLimitedTwitchChatCommand} = require('../../service/rateLimited');
 
+const dayOffsets = {
+    '昨天': -1,
+    '今天': 0,
+    '明天': 1,
+    '後天': 2
+};
+
+function getDayOffset(day) {
+    const normalizedDay = String(day || '').trim();
+
+    if (Object.prototype.hasOwnProperty.call(dayOffsets, normalizedDay)) {
+        return dayOffsets[normalizedDay];
+    }
+
+    return 0;
+}
+
 class FifaToday extends RateLimitedTwitchChatCommand {
     constructor(client) {
         super(client, {
             name: '!fifa今天',
             aliases: ['!FIFA今天', '!世足今天','!fifa','!FIFA','!世足'],
             group: 'querys',
-            description: 'querys fifa today scores'
+            description: 'querys fifa today scores',
+            args: [
+                {
+                    name: 'day'
+                }
+            ]
         });
     }
 
-    async delayRun(msg) {
+    async delayRun(msg, { day } = {}) {
         try {
-            const messages = await fifaSvc.getMessagesForDay(0);
+            const messages = await fifaSvc.getMessagesForDay(getDayOffset(day));
 
             messages.forEach(message => {
                 msg.reply(message);
